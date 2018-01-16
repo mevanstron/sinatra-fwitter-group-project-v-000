c49a0eece0d236bf68b1961d4b68abc13df20f72
Michael Evans <michael@michaelgregoryevans.com>
2018-01-16 21:45:07 +0000

    automatically backed up by learn

--- app/controllers/tweet_controller.rb
+++ app/controllers/tweet_controller.rb
@@ -37,15 +37,16 @@ class TweetController < ApplicationController
   end
 
   get '/tweets/:id/edit' do
-    binding.pry
-    @tweet = Tweet.find(params[:id])
-    #if !Helpers.logged_in?(session)
+    if Helpers.logged_in?(session)
+      @tweet = Tweet.find(params[:id])
+      if Helpers.current_user(session).id == @tweet.user_id
+        erb :'tweets/edit_tweet'
+      else
+        redirect to '/tweets'
+      end
+    else
       redirect to '/login'
-    #elsif Helpers.current_user(session).id == @tweet.user_id
-      #erb :'tweets/edit_tweet'
-    #else
-      #redirect to '/tweets'
-    #end
+    end
   end
 
   patch '/tweets/:id' do
